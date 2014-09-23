62bd8b03f19141b21f6d4d5284fb08ca611cf7e9
Bruno Souza <varnion@yandex.com>
2014-09-23 16:54:33 -0300

    scripts from 3rd and 4th days

---
+++ README.md
@@ -0,0 +1,5 @@
+Scripts based on dna and rna-seq classes from "Introduction to Bioinformatics"
+presented by Paul Boutros, Fouad Yousif and Julie Livingstone - Ontario Institute of Cancer Research (OICR, U. Toronto)
+
+September 2014
+Instituto de Química - Universidade de São Paulo
---
+++ rna-seq.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+### setting up the directory
+
+RNA_HOME=$HOME/cursoOntario/RNA/rna-seq_tutorial
+RNA_DATA_DIR=$RNA_HOME/data
+TRANS_IDX_DIR=$RNA_HOME/alignments/tophat/trans_idx
+
+mkdir -p $TRANS_IDX_DIR
+
+# ### get the data
+# ### index the reference genome
+# 
+# bowtie2-build $RNA_HOME/refs/hg19/bwt/9/9/9.fa
+# 
+# ## ==================== ##
+# ## ==== run tophat2 === ##
+# ## ==================== ##
+#
+# cd $RNA_HOME/alignments/tophat 
+# 
+# tophat2 -p 8 --mate-inner-dist 80 --mate-std-dev 38 \
+#     --segment-length 18 --rg-id=normal --rg-sample=normal_N02 \
+#     -o normal_N02 -G $RNA_HOME/refs/hg19/genes/genes_chr9.gtf \
+#     --transcriptome-index $TRANS_IDX_DIR/ENSG_Genes $RNA_HOME/refs/hg19/bwt/9/9 \
+#     $RNA_DATA_DIR/normal_N02_read1.fasta $RNA_DATA_DIR/normal_N02_read2.fasta
+# 
+# tophat2 -p 8 --mate-inner-dist 80 --mate-std-dev 38 \
+#     --segment-length 18 --rg-id=normal --rg-sample=normal_N03 \
+#     -o normal_N03 -G $RNA_HOME/refs/hg19/genes/genes_chr9.gtf \
+#     --transcriptome-index $TRANS_IDX_DIR/ENSG_Genes $RNA_HOME/refs/hg19/bwt/9/9 \
+#     $RNA_DATA_DIR/normal_N03_read1.fasta $RNA_DATA_DIR/normal_N03_read2.fasta
+# 
+# 
+# tophat2 -p 8 --mate-inner-dist 80 --mate-std-dev 38 \
+#     --segment-length 18 --rg-id=normal --rg-sample=normal_N06 \
+#     -o normal_N06 -G $RNA_HOME/refs/hg19/genes/genes_chr9.gtf \
+#     --transcriptome-index $TRANS_IDX_DIR/ENSG_Genes $RNA_HOME/refs/hg19/bwt/9/9 \
+#     $RNA_DATA_DIR/normal_N06_read1.fasta $RNA_DATA_DIR/normal_N06_read2.fasta
+# 
+# 
+# tophat2 -p 8 --mate-inner-dist 80 --mate-std-dev 38 \
+#     --segment-length 18 --rg-id=carcinoma --rg-sample=carcinoma_C02 \
+#     -o carcinoma_C02 -G $RNA_HOME/refs/hg19/genes/genes_chr9.gtf \
+#     --transcriptome-index $TRANS_IDX_DIR/ENSG_Genes $RNA_HOME/refs/hg19/bwt/9/9 \
+#     $RNA_DATA_DIR/carcinoma_C02_read1.fasta $RNA_DATA_DIR/carcinoma_C02_read1.fasta
+# 
+# 
+# tophat2 -p 8 --mate-inner-dist 80 --mate-std-dev 38 \
+#     --segment-length 18 --rg-id=carcinoma --rg-sample=carcinoma_C03 \
+#     -o carcinoma_C03 -G $RNA_HOME/refs/hg19/genes/genes_chr9.gtf \
+#     --transcriptome-index $TRANS_IDX_DIR/ENSG_Genes $RNA_HOME/refs/hg19/bwt/9/9 \
+#     $RNA_DATA_DIR/carcinoma_C03_read1.fasta $RNA_DATA_DIR/carcinoma_C03_read1.fasta
+# 
+# 
+# tophat2 -p 8 --mate-inner-dist 80 --mate-std-dev 38 \
+#     --segment-length 18 --rg-id=carcinoma --rg-sample=carcinoma_C06 \
+#     -o carcinoma_C06 -G $RNA_HOME/refs/hg19/genes/genes_chr9.gtf \
+#     --transcriptome-index $TRANS_IDX_DIR/ENSG_Genes $RNA_HOME/refs/hg19/bwt/9/9 \
+#     $RNA_DATA_DIR/carcinoma_C06_read1.fasta $RNA_DATA_DIR/carcinoma_C06_read1.fasta
+# 
+# 
+# ## ==================== ##
+# ## ==== cufflinks ===== ##
+# ## ==================== ##
+#
+
+mkdir $RNA_HOME/expression
+cd $RNA_HOME/expression
+
+
