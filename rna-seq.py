f26de8f42d231de104b4364e0022abf262ae7de5
Bruno Souza <varnion@yandex.com>
2014-10-15 15:40:58 -0300

    made rna-seq.py compatible with fastq from SRA

--- rna-seq.py
+++ rna-seq.py
@@ -12,6 +12,7 @@ __email__ = "fsouza dot bruno at usp dot br"
 
 
 import os
+from collections import OrderedDict
 
 ### setting up the directories
 ### EDIT rna_home to match the path/to/your rna-seq_tutorial folder
@@ -26,6 +27,20 @@ os.system('mkdir -p '+ trans_idx_dir)
 ### index the reference genome
 
 # os.system(bowtie2-build rna_home/refs/hg19/bwt/9/9/9.fa 9)
+
+
+
+samples = OrderedDict()
+runs = {}
+
+for line in open('chosen_samples'):
+    grupo, sample, run_names = line.split()
+    if grupo not in samples: 
+        samples[grupo] = []
+    else:
+        samples[grupo].append(sample)
+        runs[sample] = run_names.split(",")
+
 
 ## ==================== ##
 ## ====== tophat2 ===== ##
@@ -42,22 +57,28 @@ genome_fasta = rna_home + "/refs/hg19/fasta/9/9.fa"
 
 os.chdir(th_dir)
 
-samples = { 'normal'    : ['_N02', '_N03', '_N06'],
-            'carcinoma' : ['_C02', '_C03', '_C06'] }
+
+
+
 
 for group in samples:
     for sample in samples[group]:
-        tophat = "tophat2 -p 8" +\
-            " --mate-inner-dist 80" +\
-            " --mate-std-dev 38" +\
-            " --segment-length 18" +\
-            " --rg-id={0} --rg-sample={0}{1}" +\
-            " -o {0}{1} -G {2}" +\
-            " --transcriptome-index {3} {5}" +\
-            " {4}/{0}{1}_read1.fasta {4}/{0}{1}_read2.fasta"
+        reads1 = ",".join([ "{}/{}_1.fastq.gz".format("fastq_folder", file) for file in runs[sample] ])
+        reads2 = ",".join([ "{}/{}_2.fastq.gz".format("fastq_folder", file) for file in runs[sample] ])
+       
 
-        tophat = tophat.format(group, sample, ref_gtf, ensg_genes, rna_data_dir, bwt_idx)
-        # format index           0      1        2         3            4           5
+        tophat = ("tophat2 -p 8" +
+                #" --mate-inner-dist 80" +\
+                #" --mate-std-dev 38" +\
+                #" --segment-length 18" +\
+                " --rg-id={0} --rg-sample={1}" +
+                " -o {1} -G {2}" +
+                " --transcriptome-index {3} {4}" +
+                " {5} {6}" )
+
+     
+        tophat = tophat.format(group, sample, ref_gtf, ensg_genes, bwt_idx, reads1, reads2)
+        # format index           0      1        2         3          4        5      6
 
         print("[running] " + tophat)
         os.system(tophat)
@@ -74,13 +95,13 @@ os.chdir(exp_dir)
 for group in samples:
     for sample in samples[group]:
 
-        cufflinks = "cufflinks -p 8" +\
-            " -o {0} --GTF {1}" +\
-            " --no-update-check" +\
-            " {2}/{0}/accepted_hits.bam"
+        cufflinks = ("cufflinks -p 8" +
+            " -o {0} --GTF {1}" +
+            " --no-update-check" +
+            " {2}/{0}/accepted_hits.bam")
 
-        cufflinks = cufflinks.format(group + sample, ref_gtf, th_dir)
-        ## format index                    0            1       2
+        cufflinks = cufflinks.format(sample, ref_gtf, th_dir)
+        ## format index                0        1       2
 
         print("[running] " + cufflinks)
         os.system(cufflinks)
@@ -112,13 +133,14 @@ os.chdir(th_dir)
 
 cuffdiff = "cuffdiff -p 8 -L {} -o {} --no-update-check {}/merged/merged.gtf"
 
-labels = ",".join([ group.capitalize() for group in samples ])
+labels = ",".join([ group for group in samples ])
 
 cuffdiff = cuffdiff.format(labels, de_dir, exp_dir)
 
 for group in samples:
-    cuffdiff += " " + ",".join([ '{}/accepted_hits.bam'.format(group + sample) for sample in samples[group] ])
+    cuffdiff += " " + ",".join([ '{}/accepted_hits.bam'.format(sample) for sample in samples[group] ])
 
 print("[running] " + cuffdiff)
 os.system(cuffdiff)
+
 
